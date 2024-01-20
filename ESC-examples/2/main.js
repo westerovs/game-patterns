ee215ab9bdaa1ace815f920dea02a230efcfc9d5
westerovs@mail.ru <westerovs@mail.ru>
2024-01-20 08:35:35 +0300

    ECS 2nd: create empty structure

--- ESC-examples/2/main.js
+++ ESC-examples/2/main.js
@@ -1,1 +1,10 @@
 console.log('HELLO JS')
+
+// import {Game} from './game.mjs'
+
+const game = null
+
+window.addEventListener('load', (event) => {
+//   game = new Game({canvasId: 'game-canvas', loggingEnabled: false})
+//   game.start()
+})
